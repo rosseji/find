6b97ccd17539f60abfaea8c4d96f163b3cea2309
rosseji <rosseireland@gmail.com>
2017-08-08 06:02:13 +1000

    arg, breaking change to step back from dbox

--- R/this.R
+++ R/this.R
@@ -10,17 +10,11 @@
 
 this <- function(path = NULL) {
 
-  local_dir <- getwd() %>%
+   getwd() %>%
     stringr::str_split("/") %>%
     .[[1]] %>%
     .[2:3] %>%
     stringr::str_c(collapse = "/") %>%
-    paste0("/", .) %>%
-    dir(full.names = T)
+    paste0("/", ., path)
 
-  if (is.null(path)) {
-    local_dir[which(stringr::str_detect(local_dir,"Dropbox"))]
-  } else {
-    paste0(local_dir[which(stringr::str_detect(local_dir,"Dropbox"))], path)
-  }
 }
